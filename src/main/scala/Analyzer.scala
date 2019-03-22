4c2d1ebc80d4e5a14f87ae5f1ec5d1296e9979c1
Everstar <tsengkasing@gmail.com>
2019-03-23 01:01:28 +0800

    :rocket: Kafka - Spark-Streaming OK (#1)
    233

--- src/main/scala/Analyzer.scala
+++ src/main/scala/Analyzer.scala
@@ -8,8 +8,8 @@ import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.kafka010.{KafkaUtils, LocationStrategies, ConsumerStrategies}
 
-object Analyzer {
-  val logger: Logger = Logger.getLogger(Analyzer.getClass)
+object Analyzer extends Serializable {
+  @transient val logger: Logger = Logger.getLogger(Analyzer.getClass)
   // 关键词
   val keywords: Seq[String] = Seq("天气", "污染", "空气", "呼吸", "肺部", "烟雾", "雾霾", "咳嗽")
 
@@ -18,33 +18,48 @@ object Analyzer {
     */
   def loadFromKafka() : Unit = {
     val conf = new SparkConf().setAppName("AirQualitySensor")
-    val ssc = new StreamingContext(conf, Seconds(1)) // streaming 的时间间隔, 当前为 1 秒
+    val ssc = new StreamingContext(conf, Seconds(10)) // streaming 的时间间隔, 当前为 1 秒
 
     val kafkaParams = Map[String, Object](
-      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "10.42.2.25:2181", // broker
-      //      ConsumerConfig.GROUP_ID_CONFIG -> groupId,
-      ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "earliest",
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "10.42.2.25:9092", // broker
+      ConsumerConfig.GROUP_ID_CONFIG -> "0",
+      ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "latest",
       ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
-      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer])
+      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer]
+    )
     val topics = Set("test03")
 
-    val messages = KafkaUtils.createDirectStream[String, String](
+    val kafkaStream = KafkaUtils.createDirectStream[String, String](
       ssc,
       LocationStrategies.PreferConsistent,
       ConsumerStrategies.Subscribe[String, String](topics, kafkaParams)
     )
 
-    val lines = messages.map(_.value)
-    val words = lines.flatMap(_.split(" "))
-    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
-    wordCounts.print()
+    def hasKeywords(text: String): Boolean = {
+      logger.info(text.slice(0, 10)) // 打印前10个字符
+      for (keyword <- keywords
+           if text.contains(keyword)) {
+        return true
+      }
+      false
+    }
+
+    val lines = kafkaStream.map(record => record.value).cache()
+    // 总微博条数
+    val totalNumLines = lines.count()
+    totalNumLines.print()
+
+    val num = lines
+      .filter(hasKeywords)
+      .count()
+    num.print()
 
     print(s"\n ========================= BEGIN =========================\n\n")
     // 开始
     ssc.start()
     ssc.awaitTermination()
 
-    print(s"\n ========================= HEY!!! =========================\n\n")
+    print(s"\n ========================= !!!END!!! =========================\n\n")
   }
 
   /**
@@ -72,6 +87,9 @@ object Analyzer {
   }
   def main(args: Array[String]): Unit = {
     // 从磁盘文本读取
-    loadFromDisk("/home/hduser/app/spark-text/weibo-15M.txt")
+    // loadFromDisk("/home/hduser/app/spark-text/weibo-15M.txt")
+
+    // 从 kafka 读取
+    loadFromKafka()
   }
 }
