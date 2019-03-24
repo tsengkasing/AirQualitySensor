1ed0a02b34dc090f8e6ae80e2ef08ac7d60485a2
tsengkasing <tsengkasing@gmail.com>
2019-03-24 13:55:38 +0800

    :ghost: 流程走通

--- README.md
+++ README.md
@@ -14,13 +14,25 @@ Working In Progress.
 
 ### :beer: 运行
 
-- 先上传到集群
+- 编译打包成 Jar 包
+- 上传到集群
     ```shell
     $ scp -o ProxyCommand='ssh group05@202.45.128.135 -W %h:%p' out/artifacts/AirQualitySensor_jar/AirQualitySensor.jar hduser@gpu5:~/app/temp/
     ```
-- 到集群上运行
+- 在集群上运行
     ```shell
     $ ssh ...
     $ cd ~/app/temp
-    $ spark-submit --class AirQualitySensor.Analyzer ./AirQualitySensor.jar
+    $ spark-submit --class AirQualitySensor.Analyzer ./AirQualitySensor.jar test04
     ```
+- 开启 Kafka Producer (Option 1)
+    ```shell
+    $ kafka-console-producer.sh --broker-list gpu5:9092 --topic test04
+    $> 输入测试用的文本
+    ```
+- 开启 Flume 搬迁数据到 Kafka
+    > 配置文件里 hard code 了 topic test04
+    ```shell
+    $ flume-ng agent -c /home/hduser/app/flume-rate-controller -f rate-control-flume-conf.properties --name agent -Dflume.root.logger=INFO,console
+    ```
+    PS: 以上的 **test04** 是指 *Kafka* 的 **topic** , 可自定义(需创建)
--- src/main/scala/Analyzer.scala
+++ src/main/scala/Analyzer.scala
@@ -16,18 +16,18 @@ object Analyzer extends Serializable {
   /**
     * 从 kafka 读取
     */
-  def loadFromKafka() : Unit = {
+  def loadFromKafka(topic: String) : Unit = {
     val conf = new SparkConf().setAppName("AirQualitySensor")
     val ssc = new StreamingContext(conf, Seconds(10)) // streaming 的时间间隔, 当前为 1 秒
 
     val kafkaParams = Map[String, Object](
       ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> "10.42.2.25:9092", // broker
       ConsumerConfig.GROUP_ID_CONFIG -> "0",
-      ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "latest",
+      ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> "latest", // earliest | latest
       ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer],
       ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> classOf[StringDeserializer]
     )
-    val topics = Set("test03")
+    val topics = Set(topic)
 
     val kafkaStream = KafkaUtils.createDirectStream[String, String](
       ssc,
@@ -46,13 +46,25 @@ object Analyzer extends Serializable {
 
     val lines = kafkaStream.map(record => record.value).cache()
     // 总微博条数
-    val totalNumLines = lines.count()
-    totalNumLines.print()
-
-    val num = lines
-      .filter(hasKeywords)
+    lines
+      .map(l => (l, 1))
       .count()
-    num.print()
+      .saveAsTextFiles("file:///home/hduser/app/spark-text/total.txt") // 非 file:// 开头会写到 hdfs 上
+
+    lines
+      .filter(hasKeywords)
+      .print()
+//      .saveAsTextFiles("/home/hduser/app/spark-text/air.txt")
+//      .map(l => (l, 1))
+//      .reduceByKey((l1, l2) => l1 + l2)
+
+    // foreachRDD 在需要产生副作用的时候使用
+//    lines.foreachRDD(rdd => {
+//      if (rdd != null && !rdd.isEmpty() && !rdd.partitions.isEmpty) {
+//        val totalNum = rdd.coalesce(1, shuffle = false).count()
+//        logger.info(s"Total Num of message : $totalNum")
+//      }
+//    })
 
     print(s"\n ========================= BEGIN =========================\n\n")
     // 开始
@@ -90,6 +102,7 @@ object Analyzer extends Serializable {
     // loadFromDisk("/home/hduser/app/spark-text/weibo-15M.txt")
 
     // 从 kafka 读取
-    loadFromKafka()
+    val Array(topic) = args
+    loadFromKafka(topic)
   }
 }
